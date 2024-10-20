87d5f714610ddf831344cbe66a0c6024676fd75a
秦思成-MacBook <464815411@qq.com>
2024-10-20 11:46:47 +0800

    快捷键映射

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+-- 将 Shift-W 映射为向前移动一个单词
+vim.api.nvim_set_keymap("n", "<S-W>", "b", { noremap = true, silent = true })
+-- 将 Shift-e 映射为向后移动一个单词
+vim.api.nvim_set_keymap("n", "<S-e>", "ge", { noremap = true, silent = true })
